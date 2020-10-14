d927eeaf43c2c1bfa4fddf987b21c0e2ff5818ae
Saanbeux <moises.garip@upr.edu>
2020-10-13 23:15:01 -0400

    Added Primary DAOs for PSQL DB interaction.

---
+++ src/backend/config/dbconfig.py
@@ -0,0 +1,10 @@
+
+#Databse configuration information
+
+pg_config = {
+    'user' : 'gbxrvscvbgnrfj',
+    'passwd' : 'b4b81ffde7b3f9148b3bdc017063c4c706bef179663a188dedae12f22aa5a13f',
+    'dbname' : 'd99l1bhk1b777a',
+    'host' : 'ec2-18-209-187-54.compute-1.amazonaws.com',
+    'port' : 5432
+}
