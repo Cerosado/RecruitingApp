32bae0d7751a8451dde03aadd35c162fc43a7acd
Saanbeux <moises.garip@upr.edu>
2020-10-13 23:25:58 -0400

    Update dbconfig.py

--- src/backend/config/dbconfig.py
+++ src/backend/config/dbconfig.py
@@ -1,5 +1,6 @@
 
 #Database Access information
+#Each table has a dummy entry for debugging (applicant id 17, posting id 7, company id 20, an application between applicant 17 & 7)
 pg_config = {
     'user' : 'gbxrvscvbgnrfj',
     'passwd' : 'b4b81ffde7b3f9148b3bdc017063c4c706bef179663a188dedae12f22aa5a13f',
