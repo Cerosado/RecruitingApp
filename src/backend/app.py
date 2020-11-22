4362ab6338e6205fe53503aba13f720c0453ba14
Eduardo Perez <eduardo.perez13@upr.edu>
2020-11-22 13:14:45 -0400

    Added base flask-praetorian authentication

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,20 +1,67 @@
-from io import BytesIO
 from functools import wraps
+from io import BytesIO
+
+import flask_praetorian
+import flask_sqlalchemy
 from flask import Flask, request, flash, redirect, url_for, session, jsonify
 from flask_cors import CORS
-# from passlib.hash import sha256_crypt
-# from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-# from forms import RegisterForm, LoginForm
-from .Handlers.jobPosting import JobPostingHandler
-from .Handlers.resume import ResumeHandler
 from werkzeug.utils import secure_filename
 
+from .Handlers.jobPosting import JobPostingHandler
+from .Handlers.resume import ResumeHandler
+
+guard = flask_praetorian.Praetorian()
+
+# Initialize flask
 app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 CORS(app)
 app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'ciic4060'
+app.config['SECRET_KEY'] = 'top secret'
+app.config["JWT_ACCESS_LIFESPAN"] = {"hours": 24}
+app.config["JWT_REFRESH_LIFESPAN"] = {"days": 30}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgresql+psycopg2://gbxrvscvbgnrfj:b4b81ffde7b3f9148b3bdc017063c4c706bef179663a188dedae12f22aa5a13f@' \
+    'ec2-18-209-187-54.compute-1.amazonaws.com:5432/d99l1bhk1b777a'
+db = flask_sqlalchemy.SQLAlchemy(app)
+db.Model.metadata.reflect(db.engine)
 
 
+# SQLAlchemy model used by flask-praetorian to access accounts table
+class User(db.Model):
+    __table__ = db.Model.metadata.tables['accounts']
+
+    @property
+    def identity(self):
+        return self.user_id
+
+    @property
+    def rolenames(self):
+        try:
+            return self.roles.split(",")
+        except Exception:
+            return []
+
+    @classmethod
+    def lookup(cls, username):
+        return cls.query.filter_by(username=username).one_or_none()
+
+    @classmethod
+    def identify(cls, id):
+        return cls.query.get(id)
+
+    def is_valid(self):
+        return self.is_active
+
+
+# Initialize the flask-praetorian instance for the app
+guard.init_app(app, User)
+
+
+###########################################
+#             Job Postings                #
+###########################################
 @app.route('/JobPosting/<int:posting_id>', methods=['GET'])
 def jobPostingDetail(posting_id):
     if request.method == 'GET':
@@ -66,6 +113,55 @@ def is_logged_in(f):
             flash('Unauthorized, please log in.', 'danger')
             return redirect(url_for('login'))
     return wrap
+
+
+###########################################
+#             Authentication              #
+###########################################
+@app.route('/api/login', methods=['POST'])
+def login():
+    """
+    Logs a user in by parsing a POST request containing user credentials and
+    issuing a JWT token.
+    .. example::
+       $ curl http://localhost:5000/api/login -X POST \
+         -d '{"username":"Yasoob","password":"strongpassword"}'
+    """
+    req = request.form
+    username = req.get('username', None)
+    password = req.get('password', None)
+    user = guard.authenticate(username, password)
+    ret = {'access_token': guard.encode_jwt_token(user)}
+    return ret, 200
+
+
+@app.route('/api/refresh', methods=['POST'])
+def refresh():
+    """
+    Refreshes an existing JWT by creating a new one that is a copy of the old
+    except that it has a refrehsed access expiration.
+    .. example::
+       $ curl http://localhost:5000/api/refresh -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    print("refresh request")
+    old_token = request.get_data()
+    new_token = guard.refresh_jwt_token(old_token)
+    ret = {'access_token': new_token}
+    return ret, 200
+
+
+@app.route('/api/protected')
+@flask_praetorian.roles_required("applicant")
+def protected():
+    """
+    A protected endpoint. The auth_required decorator will require a header
+    containing a valid JWT
+    .. example::
+       $ curl http://localhost:5000/api/protected -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    return jsonify(message=f'protected endpoint (allowed user {flask_praetorian.current_user().username})')
 
 
 # @app.route('/logout')
