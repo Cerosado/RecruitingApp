09d3504d9c90f8cf8634e801a6e89001f363dcf3
Eduardo Perez <eduardo.perez13@upr.edu>
2020-11-22 22:08:38 -0400

    Added WIP authentication to react app with react-token-auth.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -12,34 +12,46 @@ import ApplicantsList from "./Applicants";
 import JobPostingsList from "./CompanyPostings";
 import PrimarySearchAppBar from "./NavBar";
 import {Container} from "@material-ui/core";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 class App extends React.Component{
-  render() {
-    return (
-        <Router>
-          <div>
-            <PrimarySearchAppBar/>
-            <Container maxWidth={"md"}>
-              <Switch>
-                <Route path="/JobPostings/:id"
-                       render={routerProps => (
-                           <ApplicantsList {...routerProps}/>
-                       )}>
-                </Route>
-                <Route path="/JobPostings"
-                       render={routerProps => (
-                           <JobPostingsList {...routerProps}/>
-                       )}>
-                </Route>
-                <Route path="/">
-                  <JobPostingsList/>
-                </Route>
-              </Switch>
-            </Container>
-          </div>
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <PrimarySearchAppBar/>
+                    <Container maxWidth={"md"}>
+                        <Switch>
+                            <Route path="/JobPostings/:id"
+                                   render={routerProps => (
+                                       <ApplicantsList {...routerProps}/>
+                                   )}>
+                            </Route>
+                            <Route path="/JobPostings"
+                                   render={routerProps => (
+                                       <JobPostingsList {...routerProps}/>
+                                   )}>
+                            </Route>
+                            <Route path="/Login"
+                                   render={routerProps => (
+                                       <SignIn {...routerProps}/>
+                                   )}>
+                            </Route>
+                            <Route path="/SignUp"
+                                   render={routerProps => (
+                                       <SignUp {...routerProps}/>
+                                   )}>
+                            </Route>
+                            <Route path="/">
+                                <JobPostingsList/>
+                            </Route>
+                        </Switch>
+                    </Container>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
